ccd29457e1878d396e24f258177b710d4f441ce0
jijianfeng <1065109432@qq.com>
2022-04-15 15:33:48 +0800

    1

---
+++ api.py
@@ -0,0 +1,18 @@
+# coding = utf-8
+# NAME: api
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/api/test')
+def hello_world():
+    '''
+    测试接口
+    :return: success
+    '''
+    return 'success'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
